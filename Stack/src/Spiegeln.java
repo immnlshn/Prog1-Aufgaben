4733bf6101e4d3aa4672bcea5822963156f637f1
Immanuel Sohn <info@immanuelsohn.de>
2022-12-05 10:29:06 +0100

    u11a02

---
+++ Stack/src/Spiegeln.java
@@ -0,0 +1,22 @@
+public class Spiegeln {
+
+    public static void main(String[] args) {
+        String s = "hallo";
+        System.out.println(s + " gespiegelt ist " + spiegeln(s));
+    }
+    public static String spiegeln(String s){
+        if(s.length() == 0){
+            return "";
+        }
+        Stack st = new Stack(s.length());
+        for(int i = 0; i < s.length(); i++){
+            st.push(s.charAt(i));
+        }
+        String out = "";
+        while(!st.isEmpty()){
+            out += st.pop();
+            System.out.println(out);
+        }
+        return out;
+    }
+}
